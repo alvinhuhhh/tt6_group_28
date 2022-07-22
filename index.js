9013928629d739c9e4d53963cd53b6c8b175d584
shanisepst <shanisepst@gmail.com>
2022-07-22 13:38:10 +0800

    Added API to view user's currency wallets

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const express = require("express");
 const app = express();
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
-  password: "1234",
+  password: "",
   database: "multicurrency",
 });
 
 connection.connect((error) => {
   if (error) {
-    return console.log("Error connecting to MySQL Database.");
+    return console.log(error);
   }
   console.log("Connection established successfully.");
 });
 
 // GET request to view all of the user's wallets
 app.get("/viewWallets", walletController.viewAllCurrencyWallets);
-// GET request for user to view a requested particular wallet
-app.get("/viewWallets/:wallet", walletController.viewReqCurrencyWallet);
--- walletController.js
+++ walletController.js
@@ -17,23 +17,3 @@ exports.viewAllCurrencyWallets = (req, res) => {
     });
   });
 };
-
-// For discussion with person doing Frontend [3] - if function is required
-// This function returns a response with the user's a specified currecy wallet
-exports.viewReqCurrencyWallet = (req, res) => {
-  const username = req.body.username;
-  const walletType = req.body.walletType;
-
-  const sql = `SELECT u.id, u.name, w.name FROM user u LEFT JOIN wallet w on u.id = w.user_id WHERE u.name = ${username} AND w.name = ${walletType}`;
-  connection.query(sql, (error, results, fields) => {
-    if (error) {
-      return console.error("You do not have any currency wallets.");
-    }
-    res.status(200).json({
-      status: 200,
-      data: {
-        results,
-      },
-    });
-  });
-};

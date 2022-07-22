cb75aee1bdff7d79eb3846d0b097ac56e53e8d8e
shanisepst <shanisepst@gmail.com>
2022-07-22 16:59:46 +0800

    Added error handling for requests

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "12345",
+  password: "calcle37",
   database: "multicurrency",
 });
 
@@ -35,7 +35,6 @@ viewAllCurrencyWallets = (req, res) => {
   const sql = `SELECT w.name, c.currency, c.amount FROM user u LEFT JOIN wallet w ON u.id = w.user_id LEFT JOIN currency c ON w.id = c.wallet_id WHERE u.username = '${username}'`;
   connection.query(sql, (error, results, fields) => {
     if (error) {
-      throw error;
       return res.status(400).json({
         status: "fail",
         message: "Invalid request.",
@@ -68,7 +67,10 @@ app.get("/getExchangeRate", (req, res) => {
   let sql = `SELECT * FROM exchange_rate`;
   connection.query(sql, (err, result) => {
     if (err) {
-      throw err;
+      return res.status(500).json({
+        status: "fail",
+        message: "Internal server error.",
+      });
     } else {
       res.status(200).send(result);
     }
@@ -80,7 +82,15 @@ app.get("/getExchangeRate/:id", (req, res) => {
   let sql = `SELECT * FROM exchange_rate WHERE id = ${req.params.id}`;
   connection.query(sql, (err, result) => {
     if (err) {
-      throw err;
+      return res.status(500).json({
+        status: "fail",
+        message: "Internal server error.",
+      });
+    } else if (result.length === 0) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Exchange rate does not exist.",
+      });
     } else {
       res.status(200).send(result);
     }
@@ -93,8 +103,11 @@ app.get("/getWallet", (req, res) => {
   connection.query(
     `SELECT id from user WHERE username = '${username}'`,
     (err, result) => {
-      if (err) {
-        throw err;
+      if (err || result.length === 0) {
+        return res.status(404).json({
+          status: "fail",
+          message: "Wallet does not exist.",
+        });
       } else {
         //console.log(result);
         const id = result[0].id;
@@ -115,8 +128,18 @@ app.delete("/deletewallet/:id", (req, res) => {
   // DELETING FROM SQL TABLE
   let sql = `DELETE FROM wallet WHERE id = ${req.params.id}`;
   let query = connection.query(sql, (err, result) => {
-    if (err) throw err;
+    if (err || result.length === 0) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Failed to delete wallet.",
+      });
+    } else if (result.length === 0) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Wallet does not exist.",
+      });
+    }
     console.log(result);
-    res.status(200).send("Wallet deleted");
+    res.status(200).send("Wallet successfully deleted.");
   });
 });

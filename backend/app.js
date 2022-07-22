7ad081c02c6b5beb70ec831bff7f298ed98b6952
zhenhao <zzhenhao4@gmail.com>
2022-07-22 13:10:27 +0800

    added get wallets

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+
+app.use(express.json());
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "multicurrency"
+})
+
+db.connect((err) =>{
+    if(err){
+        throw err;
+    }else{
+        console.log('Connected');
+    }
+});
+//get user wallets
+app.get('/getWallet', (req, res) =>{
+    const username = req.body.username;
+    db.query(`SELECT id from user WHERE username = '${username}'`, (err, result) =>{
+        if(err){
+            throw err;
+        }else{
+            //console.log(result);
+            const id = result[0].id;
+            console.log(id);
+            db.query(`SELECT * from wallet where user_id = '${id}'`, (err, result)=>{
+                console.log(result);
+                res.status(200).send(result);
+            })
+        }
+    })
+})
+
+
+app.listen(3000);

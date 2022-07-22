658f24509a03b083e8c120d071fb0657615505fd
Brian Choong <bchoong001@gmail.com>
2022-07-22 12:21:00 +0800

    login

--- authenticate.js
+++ authenticate.js
@@ -16,7 +16,7 @@ function authenticate() {
 	for(var i = 0; i < users.length; i++) {
 		// check is user input matches username and password of a current index of the user array
 		if(username == user[i].username && password == user[i].password) {
-			console.log(username + " is logged in!!!")
+			console.log(username + " is logged in")
 			// stop the function if this is found to be true
 			return
 		}
@@ -26,5 +26,3 @@ function authenticate() {
 
 
 
-
-app.listen(3000, () => console.log('Server running on port: http://localhost:3000'))
